8663b26e3953d62d1c3cfbea00c7576323c686b3
Timur Inal <64088609+timurinal@users.noreply.github.com>
2025-01-04 18:23:49 +0000

    Update Compression.cs
    Removed all unused code from Compression.cs, mainly from ByteBufferReader and ByteBufferWriter, as these were classes/structs pulled from other projects

--- Compression.cs
+++ Compression.cs
@@ -36,7 +36,8 @@ public static class Compression
             }
         }
 
-        if (data.Length * sizeof(int) < buffer.Length) // Data is smaller uncompressed
+        // If uncompressed is smaller, store uncompressed data.
+        if (data.Length * sizeof(int) < buffer.Length)
         {
             buffer.ClearBytes();
             buffer.WriteByte(0);
@@ -50,6 +51,7 @@ public static class Compression
         }
         else
         {
+            // Prepend a '1' to mark the data as compressed
             buffer.InsertByte(0, 1);
             return buffer.GetAllBytes().ToArray();
         }
@@ -71,7 +73,9 @@ public static class Compression
                 T value = reader.ReadObj<T>();
 
                 for (int i = 0; i < repeatCount; i++)
+                {
                     decompressed.Add(value);
+                }
             }
         }
         else
@@ -88,7 +92,6 @@ public static class Compression
     struct ByteBuffer
     {
         private List<byte> bytes;
-
         public int Length => bytes.Count;
 
         public ByteBuffer()
@@ -101,40 +104,22 @@ public static class Compression
             this.bytes = new List<byte>(bytes);
         }
 
-        public ByteBuffer(List<byte> bytes)
-        {
-            this.bytes = new List<byte>(bytes);
-        }
-
         public void InsertByte(int index, byte value)
         {
             bytes.Insert(index, value);
         }
 
         public void WriteByte(byte v) => bytes.Add(v);
-        public void WriteFloat(float v) => bytes.AddRange(BitConverter.GetBytes(v));
-        public void WriteInt16(Int16 v) => bytes.AddRange(BitConverter.GetBytes(v));
-        public void WriteInt32(Int32 v) => bytes.AddRange(BitConverter.GetBytes(v));
-        public void WriteInt64(Int64 v) => bytes.AddRange(BitConverter.GetBytes(v));
-        public void WriteUInt16(UInt16 v) => bytes.AddRange(BitConverter.GetBytes(v));
-        public void WriteUInt32(UInt32 v) => bytes.AddRange(BitConverter.GetBytes(v));
-        public void WriteUInt64(UInt64 v) => bytes.AddRange(BitConverter.GetBytes(v));
-        public void WriteBool(bool v) => bytes.AddRange(BitConverter.GetBytes(v));
-        public void WriteChar(char v) => bytes.AddRange(BitConverter.GetBytes(v));
-        public void WriteString(string v) => bytes.AddRange(System.Text.Encoding.UTF8.GetBytes(v));
-
-        public void WriteSizedString(string v)
-        {
-            var stringBytes = System.Text.Encoding.UTF8.GetBytes(v);
-            WriteInt32(stringBytes.Length);
-            bytes.AddRange(stringBytes);
-        }
 
         public void WriteObj<T>(T v) where T : struct
         {
             byte[] tBytes = StructToBytes(v);
             bytes.AddRange(tBytes);
         }
+
+        public void ClearBytes() => bytes.Clear();
+
+        public List<byte> GetAllBytes() => new List<byte>(bytes);
 
         public T ReadObj<T>(int position) where T : struct
         {
@@ -144,69 +129,36 @@ public static class Compression
         }
 
         public byte ReadByte(int position) => bytes[position];
-        public float ReadFloat(int position) => BitConverter.ToSingle(bytes.ToArray(), position);
-        public Int16 ReadInt16(int position) => BitConverter.ToInt16(bytes.ToArray(), position);
-        public Int32 ReadInt32(int position) => BitConverter.ToInt32(bytes.ToArray(), position);
-        public Int64 ReadInt64(int position) => BitConverter.ToInt64(bytes.ToArray(), position);
-        public UInt16 ReadUInt16(int position) => BitConverter.ToUInt16(bytes.ToArray(), position);
-        public UInt32 ReadUInt32(int position) => BitConverter.ToUInt32(bytes.ToArray(), position);
-        public UInt64 ReadUInt64(int position) => BitConverter.ToUInt64(bytes.ToArray(), position);
-        public bool ReadBool(int position) => BitConverter.ToBoolean(bytes.ToArray(), position);
-        public char ReadChar(int position) => BitConverter.ToChar(bytes.ToArray(), position);
-
-        public string ReadString(int position, int length) =>
-            System.Text.Encoding.UTF8.GetString(bytes.ToArray(), position, length);
-
-        public string ReadSizedString(int position)
-        {
-            int length = ReadInt32(position);
-            return ReadString(position + sizeof(int), length);
-        }
-
-        public void ClearBytes() => bytes.Clear();
-
-        public List<byte> GetAllBytes() => new List<byte>(bytes);
-
-        public ByteBufferReader GetReader() => new ByteBufferReader(this);
 
         static byte[] StructToBytes<T>(T obj) where T : struct
         {
             int size = Marshal.SizeOf(obj);
-            byte[] bytes = new byte[size];
+            byte[] result = new byte[size];
 
-            Span<byte> span = bytes;
-            MemoryMarshal.Write(span, obj);
+            Span<byte> span = result;
+            MemoryMarshal.Write(span, ref obj);
 
-            return bytes;
+            return result;
         }
 
-        static T BytesToStruct<T>(byte[] bytes) where T : struct
+        static T BytesToStruct<T>(byte[] data) where T : struct
         {
-            int size = Marshal.SizeOf(typeof(T));
+            int size = Marshal.SizeOf<T>();
+            if (data.Length != size)
+                throw new ArgumentException("Byte array size mismatch.");
 
-            if (bytes.Length != size) throw new ArgumentException("Byte array size mismatch");
-
-            return MemoryMarshal.Read<T>(bytes);
+            return MemoryMarshal.Read<T>(data);
         }
     }
 
     class ByteBufferReader
     {
-        private ByteBuffer _buffer;
-
+        private readonly ByteBuffer _buffer;
         private int _readerPosition;
-
-        public ByteBufferReader(ByteBuffer buffer)
-        {
-            _buffer = buffer;
-
-            _readerPosition = 0;
-        }
 
         public ByteBufferReader(byte[] bytes)
         {
             _buffer = new ByteBuffer(bytes);
-
             _readerPosition = 0;
         }
 
@@ -217,92 +169,6 @@ public static class Compression
             return v;
         }
 
-        public float ReadFloat()
-        {
-            var v = _buffer.ReadFloat(_readerPosition);
-            int size = sizeof(float);
-            SkipBytes(size);
-            return v;
-        }
-
-        public Int16 ReadInt16()
-        {
-            var v = _buffer.ReadInt16(_readerPosition);
-            int size = sizeof(Int16);
-            SkipBytes(size);
-            return v;
-        }
-
-        public Int32 ReadInt32()
-        {
-            var v = _buffer.ReadInt32(_readerPosition);
-            int size = sizeof(Int32);
-            SkipBytes(size);
-            return v;
-        }
-
-        public Int64 ReadInt64()
-        {
-            var v = _buffer.ReadInt64(_readerPosition);
-            int size = sizeof(Int64);
-            SkipBytes(size);
-            return v;
-        }
-
-        public UInt16 ReadUInt16()
-        {
-            var v = _buffer.ReadUInt16(_readerPosition);
-            int size = sizeof(UInt16);
-            SkipBytes(size);
-            return v;
-        }
-
-        public UInt32 ReadUInt32()
-        {
-            var v = _buffer.ReadUInt32(_readerPosition);
-            int size = sizeof(UInt32);
-            SkipBytes(size);
-            return v;
-        }
-
-        public UInt64 ReadUInt64()
-        {
-            var v = _buffer.ReadUInt64(_readerPosition);
-            int size = sizeof(UInt64);
-            SkipBytes(size);
-            return v;
-        }
-
-        public bool ReadBool()
-        {
-            var v = _buffer.ReadBool(_readerPosition);
-            SkipBytes(1);
-            return v;
-        }
-
-        public char ReadChar()
-        {
-            var v = _buffer.ReadChar(_readerPosition);
-            int size = sizeof(char);
-            SkipBytes(size);
-            return v;
-        }
-
-        public string ReadString(int length)
-        {
-            var v = _buffer.ReadString(_readerPosition, length);
-            SkipBytes(length);
-            return v;
-        }
-
-        public string ReadSizedString()
-        {
-            int length = _buffer.ReadInt32(_readerPosition);
-            var v = _buffer.ReadSizedString(_readerPosition);
-            SkipBytes(sizeof(Int32) + length);
-            return v;
-        }
-
         public T ReadObj<T>() where T : struct
         {
             var v = _buffer.ReadObj<T>(_readerPosition);
@@ -310,12 +176,11 @@ public static class Compression
             return v;
         }
 
-        public void SkipBytes(int count) => _readerPosition += count;
-        public void SetReaderPosition(int position) => _readerPosition = position;
-
         public bool IsAtEndOfBuffer()
         {
             return _readerPosition >= _buffer.Length;
         }
+
+        private void SkipBytes(int count) => _readerPosition += count;
     }
 }
